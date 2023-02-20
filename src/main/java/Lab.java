eee0bff87fbf99cea94b1f6ca213ef42e9134c3c
rriess1 <robertwriess@yahoo.com>
2023-02-20 18:48:04 +0000

    first

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -30,6 +30,11 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        String s = "";
+
+        for(int i: nums){
+            s = i + " ";
+        }
+        return s;
     }
 }
